2c779e9621d3f4aa5d49d4d6225e79a3d0690800
sanjaykshebbar <sanjaykshebbar@gmail.com>
2024-01-28 21:55:58 +0530

    This is the second commit where I have added the Indexing feature of the found sctipts and also this is capable of finding the my_script folder dynamically

--- master_script.sh
+++ master_script.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-# Define the folder path
-folder_path="$HOME/Desktop/my-scripts"
+# Find the folder dynamically
+folder_name="my-scripts"
+found_folder=$(find / -type d -name "$folder_name" -print -quit 2>/dev/null)
 
 # Check if the folder exists
-if [ -d "$folder_path" ]; then
-    cd "$folder_path" || exit 1
+if [ -z "$found_folder" ]; then
+    echo "Folder '$folder_name' not found."
+    exit 1
+fi
 
-    # List all shell scripts with index
-    echo "Available Shell Scripts:"
-    scripts=( *.sh )
-    for index in "${!scripts[@]}"; do
-        echo "$index - ${scripts[index]}"
-    done
+cd "$found_folder" || exit 1
 
-    # Ask user for script execution
-    read -p "Enter the index of the script you want to execute (or 'exit' to quit): " script_index
+# List all shell scripts with index
+echo "Available Shell Scripts:"
+scripts=( *.sh )
+for index in "${!scripts[@]}"; do
+    echo "$index - ${scripts[index]}"
+done
 
-    if [ "$script_index" != "exit" ]; then
-        # Check if the entered index is valid
-        if [ "$script_index" -ge 0 ] && [ "$script_index" -lt "${#scripts[@]}" ]; then
-            # Execute the script
-            selected_script="${scripts[script_index]}"
-            chmod +x "$selected_script"
-            ./"$selected_script"
-        else
-            echo "Invalid index!"
-        fi
+# Ask user for script execution
+read -p "Enter the index of the script you want to execute (or 'exit' to quit): " script_index
+
+if [ "$script_index" != "exit" ]; then
+    # Check if the entered index is valid
+    if [ "$script_index" -ge 0 ] && [ "$script_index" -lt "${#scripts[@]}" ]; then
+        # Execute the script
+        selected_script="${scripts[script_index]}"
+        chmod +x "$selected_script"
+        ./"$selected_script"
     else
-        echo "Exiting..."
+        echo "Invalid index!"
     fi
 else
-    echo "Folder not found!"
+    echo "Exiting..."
 fi
